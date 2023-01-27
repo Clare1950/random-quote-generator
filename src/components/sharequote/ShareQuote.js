a16687fb7451ec4bc2c5d5d6c5fcc56909e23e8b
Clare Parkes <clare.parkes@gmail.com>
2023-01-27 18:27:27 +0000

    Add tweet button

--- src/components/sharequote/ShareQuote.js
+++ src/components/sharequote/ShareQuote.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-export const ShareQuote = () => {
+export const ShareQuote = ({ quote }) => {
+  const twitterLink = "https://twitter.com/intent/tweet?text=";
+  const quoteLink = quote.split(" ").join('%20');
+const shareLink = twitterLink + quoteLink
   return (
-    <a href="twitter.com" target="_blank">
+    <a href={shareLink} target="_blank">
       Share Quote
     </a>
   );
